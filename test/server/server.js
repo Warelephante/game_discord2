6ce14449ba7f04f9293c6b7c8c9b1b08bdfee524
Warelephante <filippramsey@icloud.com>
2026-05-13 22:16:43 -0400

    Refactor server.js for improved structure and clarity

--- test/server/server.js
+++ test/server/server.js
@@ -323,7 +323,7 @@ const clientPath = path.join(__dirname, "../client/dist");
 
 app.use(express.static(clientPath));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(clientPath, "index.html"));
 });
 
